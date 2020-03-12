e5602ccd1ff8a3d6b182e4db58e90e7076900350
toyamanya <ryotoyamaiii@gmail.com>
2020-03-12 18:54:32 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type City struct {
+	ID          int    `json:"id,omitempty" db:"ID"`
+	Name        string `json:"name,omitermpty" db:"Name"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty" db:"Population"`
+}
+
+func main() {
+	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	if err != nil {
+		log.Fatalf("Cannot Connect to Database: %s", err)
+	}
+
+	fmt.Println("Connected!")
+	city := City{}
+	db.Get(&city, "SELECT * FROM city WHERE Name='Tokyo'")
+
+	fmt.Printf("Tokyoの人口は%d人です\n", city.Population)
+}
